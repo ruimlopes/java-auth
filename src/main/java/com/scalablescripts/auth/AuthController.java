aff7ff1c81eb628454c598375c342eb05860d07d
ruilopes <rui.miguel.coimbra.lopes@gmail.com>
2022-03-06 18:05:00 +0100

    Step 5 - First controller, security disabled.

---
+++ src/main/java/com/scalablescripts/auth/AuthController.java
@@ -0,0 +1,15 @@
+package com.scalablescripts.auth;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api")
+public class AuthController {
+
+    @GetMapping(value = "/hello")
+    public String hello() {
+        return "Hello!";
+    }
+}
