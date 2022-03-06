5d1cf9108086281b1842ef5af01dad30b47de6e6
ruilopes <rui.miguel.coimbra.lopes@gmail.com>
2022-03-06 18:24:15 +0100

    Step 6 - Register endpoint.

--- src/main/java/com/scalablescripts/auth/AuthController.java
+++ src/main/java/com/scalablescripts/auth/AuthController.java
@@ -1,15 +1,23 @@
 package com.scalablescripts.auth;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/api")
 public class AuthController {
+    private final UserRepo userRepo;
+
+    public AuthController(UserRepo userRepo) {
+        this.userRepo = userRepo;
+    }
 
     @GetMapping(value = "/hello")
     public String hello() {
         return "Hello!";
+    }
+
+    @PostMapping(value = "/register")
+    public User register(@RequestBody User user) {
+        return userRepo.save(user);
     }
 }
--- src/main/java/com/scalablescripts/auth/config/SecurityConfig.java
+++ src/main/java/com/scalablescripts/auth/config/SecurityConfig.java
@@ -8,7 +8,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests()
+        http.csrf().disable()
+                .authorizeHttpRequests()
                 .antMatchers("/**")
                 .permitAll();
     }
